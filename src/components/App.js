cac40a51243e80fe9afc05f5d8eb71aa93fcad7b
Michelle Gonzalez <mixesthe@gmail.com>
2021-01-12 19:42:51 -0500

    m

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
