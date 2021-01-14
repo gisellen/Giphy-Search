8c749fc65ac2428a9aac4c7e3ff3998bab1fe394
Gisellen <giselle.domingo.x@gmail.com>
2021-01-13 19:20:44 -0500

    Search can find gifs

--- src/components/App.js
+++ src/components/App.js
@@ -1,67 +1,18 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
+import SearchField from './SearchField';
 
-class App extends React.Component {
-  constructor(props) {
+export default class App extends Component {
+  constructor(props){
     super(props);
     this.state = {
-      error: null,
-      isLoaded: false,
-      gif: [],
-    };
+      value: '',
+    }
   }
-
-  //   componentDidUpdate(prevProps, prevState){
-  //     if (prevProps.gif !== this.props.gif) {
-  //         this.componentDidMount();
-  //       }
-  // }
-
-  componentDidMount() {
-    this.setState({
-      isLoaded: false,
-    });
-
-    fetch("http://api.giphy.com/v1/gifs/search?q=CAR&api_key=hNr7dizMwEhAZbcrICxMPPESXDlciYAl")
-      .then((response) => {
-        if (response.status !== 200) {
-          throw new Error("No Result");
-        }
-        return response.json();
-      })
-      .then((result) => {
-        this.setState({
-          isLoaded: true,
-          gif: [],
-        });
-        if (result.length !== 0) {
-          this.setState({
-            isLoaded: true,
-            gif: result.data,
-          });
-          // console.log(this.state.gif)
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        this.setState({
-          error,
-          gif: [],
-          isLoaded: true,
-        });
-      });
-  }
-
   render() {
-    const { error, isLoaded, gif } = this.state;
-
     return (
       <div>
-            {gif.map((gif)=>(
-              <img src={gif.images.original.url} />
-            ))}
+        <SearchField />
       </div>
-    );
+    )
   }
 }
-
-export default App;
